a161b42afaf6385f7cd74476c1a0e6fb2dfe9a1a
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2023-12-27 11:50:25 +0530

    feat: created folder structure of backend and user auth done

---
+++ Backend/main.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome our Home");
+});
+
+const user = require("./routes/user.route");
+
+app.use("/user", user);
+
+module.exports = app;
